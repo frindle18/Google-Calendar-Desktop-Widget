3b7ebe2fc0f922d9fa0777c337817e6052cb41a8
frindle18 <sudarshannikhil18@gmail.com>
2024-07-31 00:17:05 +0530

    Cleaned up formatting of dates and times

--- quickstart.py
+++ quickstart.py
@@ -1,4 +1,5 @@
 import datetime
+from dateutil import parser # To parse the ISO 8601 date time format
 import os.path
 
 from google.auth.transport.requests import Request
@@ -9,7 +10,6 @@ from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
-
 
 def main():
   """Shows basic usage of the Google Calendar API.
@@ -38,7 +38,7 @@ def main():
     service = build("calendar", "v3", credentials=creds)
 
     # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+    now = datetime.datetime.now().isoformat() + "Z"  # 'Z' indicates UTC time
     print("Getting the upcoming 10 events")
 
     calendar_id = 'd18115024d22393f6d4ce076962381dae3225d1c8c47e711e88542bbeb60eca8@group.calendar.google.com'
@@ -62,12 +62,21 @@ def main():
 
     # Prints the start and name of the next 10 events
     for event in events:
-      start = event["start"].get("dateTime", event["start"].get("date"))
-      print(start, event["summary"])
+        start = event["start"].get("dateTime", event["start"].get("date"))
+        end = event["end"].get("dateTime", event["end"].get("date"))
+
+        start_time = parser.isoparse(start)
+        end_time = parser.isoparse(end)
+
+        formatted_start_time = start_time.strftime('%d/%m/%y %H:%M')
+        formatted_end_time = end_time.strftime('%H:%M')
+
+        summary = event.get('summary')
+
+        print(f'{formatted_start_time} - {formatted_end_time} ----- {summary}')
 
   except HttpError as error:
     print(f"An error occurred: {error}")
-
 
 if __name__ == "__main__":
   main()
